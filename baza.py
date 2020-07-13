f249bc10b4e8596a52bc218e80dbe7044c1bb953
Mosiv <aqlpp1@yandex.ru>
2020-07-13 23:19:30 +0300

    Update baza.py

--- baza.py
+++ baza.py
@@ -1,8 +1,10 @@
 products_file = open('shopping_list.txt', encoding='windows-1251')
 products = products_file.read()
+#FIXME: досмотреть
 products = products.replace('сало\n', '').title()
 products_file.close()
 
 new_shopping_list = open('new_shopping_list.txt', 'w')
 new_shopping_list.write(products)
 new_shopping_list.close()
+#TODO: добавить листы
