d874d00a279e7a376509996dc96fff3fc3f32a18
Mosiv <aqlpp1@yandex.ru>
2020-07-16 13:30:49 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,1 +1,13 @@
-
+'''Game: Guess the number'''
+import random
+a = random.randint(1,50)
+popitka = 1
+while popitka < 7:
+    b = int(input('Введите число: '))
+    if b == a:
+        print('Угадал')
+        break
+    if b > a:
+        print('Меньше')
+    if b < a:
+        print('Больше')
