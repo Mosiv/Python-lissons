d6c7d3b1064737dfee11b563b3112f14594654cc
Mosiv <aqlpp1@yandex.ru>
2020-07-16 13:58:37 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,8 +1,10 @@
 '''Game: Guess the number'''
 import random
+
 random_number = random.randint(1,50)
 attempt_counter = 0
-while attempt_counter <= 5:
+print('Игра, угадай число от 1 до 50')
+while attempt_counter < 6:
     number = int(input('Введите число: '))
     attempt_counter+=1
     if number == random_number:
@@ -12,3 +14,5 @@ while attempt_counter <= 5:
         print('Меньше')
     if number < random_number:
         print('Больше')
+    if number != random_number and attempt_counter == 6:
+        print(f'Ты проиграл число было: {random_number}')
